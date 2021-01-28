cf512684573f7575f822954b11eeadac9784af2a
Chetan Chilhate <chilhatechetan04@gmail.com>
2021-01-28 23:24:15 +0530

    user card component

--- web-component/js/main.js
+++ web-component/js/main.js
@@ -1,1 +1,8 @@
-console.log('linked');
+class UserCard extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `Chetan Chilhate`;
+    }
+}
+
+window.customElements.define('user-card', UserCard);
